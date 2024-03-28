e0075ec57f469bdef505088dcb6efaf976056771
Hiimuroo <matthieu.labille@gmail.com>
2024-03-28 21:32:27 +0100

    modified:   src/pages/Home/index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,11 +14,14 @@ import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
   const { data } = useData();
-  const last =
-    data && data.events && data.events.length > 0
-      ? data.events[data.events.length - 1]
-      : null;
-
+  
+  const last = data && data.events && data.events.length > 0
+    ? data.events.reduce((latest, current) => {
+        const latestDate = new Date(latest.date);
+        const currentDate = new Date(current.date);
+        return currentDate > latestDate ? current : latest;
+      })
+    : null;
 
   return <>
     <header>
